658470bddb3de8136da8fd5ee00c55b52167d38d
Harsjm <kingsir623@gmail.com>
2024-05-23 17:59:30 -0500

    rani

---
+++ terraform/main.tf
@@ -0,0 +1,11 @@
+module "vpc" {
+  source = "./vpc"
+}
+
+module "eks" {
+  source             = "./eks"
+  vpc_id             = module.vpc.vpc_id
+  public_subnet_ids  = module.vpc.public_subnet_ids
+  private_subnet_ids = module.vpc.private_subnet_ids
+}
+
