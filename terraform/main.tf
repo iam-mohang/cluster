654f7a77d7bfc65f0c7901f599b2bd3f4c28429f
Harsjm <kingsir623@gmail.com>
2024-05-23 17:46:42 -0500

    vpc

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,18 +4,3 @@ module "vpc" {
   public_subnet_cidr_blocks = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnet_cidr_blocks = ["10.0.3.0/24", "10.0.4.0/24"]
 }
-
-
-module "eks" {
-  source             = "./eks"
-  vpc_id             = module.vpc.vpc_id
-  public_subnet_ids  = module.vpc.public_subnet_ids
-  private_subnet_ids = module.vpc.private_subnet_ids
-}
-
-
-
-
-
-
-
