375471cf62838d71dbf769eaec2d4de93576bd44
Harsjm <kingsir623@gmail.com>
2024-05-23 18:17:12 -0500

    rav

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,20 @@
 module "vpc" {
   source = "./vpc"
+
+  // Pass variables related to VPC
+  vpc_cidr_block            = var.vpc_cidr_block
+  public_subnet_cidr_blocks = var.public_subnet_cidr_blocks
+  private_subnet_cidr_blocks = var.private_subnet_cidr_blocks
 }
 
 module "eks" {
   source             = "./eks"
+
+  // Pass VPC related outputs to EKS module
   vpc_id             = module.vpc.vpc_id
   public_subnet_ids  = module.vpc.public_subnet_ids
   private_subnet_ids = module.vpc.private_subnet_ids
 }
+
+
 
